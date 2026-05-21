4214322f9c62c9ada145282c5b06ff945270df94
hiokanbd <hiokanbd@users.noreply.github.com>
2026-05-21 08:17:53 +0800

    fix: use official gradlew, fix CI and settings.gradle.kts compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,9 @@ pluginManagement {
     }
 }
 
-dependencyResolution {
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
